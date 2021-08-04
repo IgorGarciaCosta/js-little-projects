eff1cb7e00e4a69ed776e4ea1b3a411f8869c335
IgorGarciaCosta <igonildo7@gmail.com>
2021-08-04 18:10:35 -0300

    Added popUp system to get player name

--- proj2/index.js
+++ proj2/index.js
@@ -6,9 +6,47 @@ let message = ''
 let topMessage = document.getElementById("message-el");
 let sumEl = document.querySelector("#sum-el");
 let cardEl = document.querySelector("#cards-el");
+let nameEmpty = document.querySelector(".name-empty")
+let player={
+    name :"",
+    chips: 0
+}
+
+let playerEl = document.querySelector("#player-el") 
+
 let cardCounter=0
 
 let sum = 0
+
+const overlay = document.getElementById('overlay')
+const popUp = document.querySelector(".modal")
+
+
+
+function nameSubmit(){
+    if(document.querySelector(".nameInput").value == ""){
+        console.log("aa")
+        nameEmpty.textContent = "Por favor, adote um nome"
+    }
+    else{
+        player.name = document.querySelector(".nameInput").value
+        playerEl.textContent = player.name+": "+player.chips
+        closePopUp()
+    }
+    
+}
+
+function closePopUp(){
+    if(document.querySelector(".nameInput").value == ""){
+        console.log("aa")
+        nameEmpty.textContent = "Por favor, adote um nome"
+    }
+    else{
+        overlay.classList.add("unactive")
+        modal.classList.add("unactive")
+    }
+    
+}
 
 function startGame(){
     firstCard = getRandomCard()
@@ -27,18 +65,18 @@ function renderGame(){
     sumEl.textContent = ("Soma: "+sum);
 
     if(sum<21){
-        message = "want to draw a card?"
+        message = "Tirar outra carta?"
     }
     
     else if(sum === 21){
-        message ="you've got a blackjack!"
+        message ="Uou! ganhou o blackjack!"
         sumEl.textContent =("Soma: "+sum);
         hasBJ=true;
     }
     
     else{
         isAlive = false
-        message ="u're out of the game!"
+        message ="Perdeste, meu bom"
         sumEl.textContent = ("Soma: "+sum);
     }
 
@@ -46,11 +84,13 @@ function renderGame(){
 }
 
 function addCard(){
-    if(isAlive){
+    if(isAlive&&!hasBJ){
         cardCounter++;
         let card = getRandomCard();
         sum+=card;
         cards.push(card)
+        player.chips+=5
+        playerEl.textContent = player.name+": "+player.chips
         renderGame();
     }
     
