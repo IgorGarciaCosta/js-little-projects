cbb32015d2759fa209ad3067cd04ca11ef7067b2
IgorGarciaCosta <igonildo7@gmail.com>
2021-08-04 00:38:47 -0300

    Finalizing styles

--- proj1/index.js
+++ proj1/index.js
@@ -1,10 +1,15 @@
 let count = 0;
+let saveEl = document.getElementById("save-el");
+let counter = document.getElementById("count")
 
 function increment(){
     count++;
-    document.getElementById("count").innerText = count;
+    counter.innerText = count;
 }
 
 function save(){
-
+    let savedValue = count+' - ';
+    saveEl.textContent += savedValue;
+    count = 0
+    counter.innerText = count;
 }
