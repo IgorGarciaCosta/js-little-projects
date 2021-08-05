ab086864163dd9dc555c159d696ea88c9178c788
IgorGarciaCosta <igonildo7@gmail.com>
2021-08-05 10:03:05 -0300

    adding local storage and delete button

--- proj3/index.js
+++ proj3/index.js
@@ -1,23 +1,44 @@
 const inputBtn = document.querySelector("#input-btn")
+const deleteBtn = document.querySelector("#delete-btn")
 let myLeads =[]
 const inputEl = document.querySelector("#input-el")
 const ulEl = document.querySelector("#ul-el")
 let listItems = ''
 
+
+
+
+var storedLeads = JSON.parse(localStorage.getItem("myLeads"));
+
+if(storedLeads){
+    myLeads = storedLeads
+    renderLeads()
+}
+
+
+//buttons listeners
 inputBtn.addEventListener("click", handler)
+deleteBtn.addEventListener("click", handleDelete)
 
-// $(inputBtn).ready(function(){
-//     $("#input-btn").click(handler)
-// })
 
+//buttons functions
+function handleDelete(){
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+}
 function handler(){
     myLeads.push(inputEl.value)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
     inputEl.value = ""
     renderLeads()
     console.log(myLeads)
 }
 
+
+//other functions
 function renderLeads(){
+    
     for(let i =0;i<myLeads.length;i++){
         listItems+=`
         <li>
