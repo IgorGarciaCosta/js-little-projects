24a485c5f7e7e28fcb2be7ed4811e46df2830c38
IgorGarciaCosta <igonildo7@gmail.com>
2021-08-04 22:42:43 -0300

    Adding extension to google

--- proj3/index.js
+++ proj3/index.js
@@ -4,12 +4,18 @@ const inputEl = document.querySelector("#input-el")
 const ulEl = document.querySelector("#ul-el")
 let listItems = ''
 
-inputBtn.addEventListener("click", function(){
+inputBtn.addEventListener("click", handler)
+
+// $(inputBtn).ready(function(){
+//     $("#input-btn").click(handler)
+// })
+
+function handler(){
     myLeads.push(inputEl.value)
     inputEl.value = ""
     renderLeads()
     console.log(myLeads)
-})
+}
 
 function renderLeads(){
     for(let i =0;i<myLeads.length;i++){
@@ -20,7 +26,6 @@ function renderLeads(){
             </a>
         </li>`
     }
-    $("#ul-el").empty()
     ulEl.innerHTML = listItems
     listItems=""
 }
